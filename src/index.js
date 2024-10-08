a6e5481a532d77e128a247587d29f2671e557a19
hatranduyphat2004 <hatranduyphat2004@gmail.com>
2024-10-09 02:23:16 +0700

    MVC 1

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const path = require('path');
+const route = require('./routes');
 
 const app = express();
 const PORT = 3333;
 
+// body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// config static file
 app.use(express.static(path.join(__dirname, '/public')));
 
+// http logger
 app.use(morgan('combined'));
 
+// view engine
 app.engine(
 	'hbs',
 	handlebars({
@@ -19,12 +27,12 @@ app.engine(
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '\\resources\\views'));
 
-app.get('/', function (req, res) {
-	return res.render('home');
-});
+// init routes
+route(app);
 
-app.get('/news', function (req, res) {
-	return res.render('news');
+// 404 page
+app.get('*', function (req, res) {
+	return res.render('404');
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
